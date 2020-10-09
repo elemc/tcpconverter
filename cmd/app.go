6037193f42c5bbac8a1d04db47308ff96146b873
elemc <me@elemc.name>
2020-10-09 15:39:09 +0300

    fixed argument names

--- cmd/app.go
+++ cmd/app.go
@@ -25,9 +25,9 @@ func main() {
 
 	flag.StringVar(&config.Address, "serial-addr", "/dev/ttyS0", "serial address")
 	flag.IntVar(&config.BaudRate, "baud-rate", 9600, "baud rate ")
-	flag.IntVar(&config.BaudRate, "data-bits", 8, "data bits (5, 6, 7, 8)")
-	flag.IntVar(&config.BaudRate, "stop-bit", 1, "stop bit (1 or 2)")
-	flag.StringVar(&config.Address, "parity", "N", "parity")
+	flag.IntVar(&config.DataBits, "data-bits", 8, "data bits (5, 6, 7, 8)")
+	flag.IntVar(&config.StopBits, "stop-bit", 1, "stop bit (1 or 2)")
+	flag.StringVar(&config.Parity, "parity", "N", "parity")
 	flag.DurationVar(&config.Timeout, "serial-timeout", time.Millisecond*100, "TCP timeout")
 	flag.Parse()
 
